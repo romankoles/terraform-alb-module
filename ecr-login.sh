c477b1f7b2471fe610a9d0475c2c2b8e06bf765a
Roman Koles <roman.koles@hotmail.com>
2020-08-17 19:13:14 -0400

    Update ecr-login.sh
    # as a second option from CLI:
    # aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin {{AWS-account-number}}.dkr.ecr.us-east-2.amazonaws.com`

--- ecr-login.sh
+++ ecr-login.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 REGION="us-east-2"
 `aws ecr get-login --no-include-email --region ${REGION}`
+
+# as a second option from CLI:
+# aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin {{AWS-account-number}}.dkr.ecr.us-east-2.amazonaws.com`
